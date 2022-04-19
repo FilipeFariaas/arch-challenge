3d9f6190146fc657fc2507ebc8c7546e6f205ac2
FilipeFariaas <filipefarias117@gmail.com>
2022-04-19 20:16:05 -0300

    refactor: remove unnecessary newBalance variable

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -7,10 +7,6 @@ class TransactionController {
   public async getAllAccountTransactions (req: Request, res: Response): Promise<Response> {
     const transactions = await Transaction.find()
 
-    // const balance = await Account.findById('625e09d9dea82d194642247e', { balance: 1, _id: 0 })
-    // console.log(transactions[0].transactionValue)
-    // console.log(balance)
-
     return res.json(transactions)
   }
 
@@ -19,17 +15,13 @@ class TransactionController {
 
     let { transactionValue, transactionType, account } = transaction
 
-    console.log(transactionValue)
-    console.log(transactionType)
-    console.log(account.valueOf())
-
     const { balance } = await Account.findById(account.valueOf())
 
     if (transactionType === 'debito') {
       transactionValue = -Math.abs(transactionValue)
     }
 
-    const newBalance = await Account.findByIdAndUpdate('625e09d9dea82d194642247e', {
+    await Account.findByIdAndUpdate('625e09d9dea82d194642247e', {
       balance: balance + transactionValue
     })
 
