1cfaad3fcc2d2f859d0e1cc0a84525c0fcc835ed
FilipeFariaas <filipefarias117@gmail.com>
2022-04-18 21:32:09 -0300

    fix: add account balance property

--- src/models/accountModel.ts
+++ src/models/accountModel.ts
@@ -2,12 +2,17 @@ import { Schema, model } from 'mongoose'
 
 interface AccountInterface {
   accountType: string,
-  holder: string
+  holder: string,
+  balance: number
 }
 
 const AccountSchema = new Schema({
   accountType: String,
-  holder: String
+  holder: String,
+  balance: {
+    type: Number,
+    default: 0
+  }
 })
 
 export default model<AccountInterface>('Account', AccountSchema)
