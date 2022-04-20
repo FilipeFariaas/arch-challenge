3326123341a3924d920df27f608cdebef2edeef0
FilipeFariaas <filipefarias117@gmail.com>
2022-04-19 22:02:25 -0300

    add balance in redis cache

--- src/controllers/accountControler.ts
+++ src/controllers/accountControler.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 
 import Account from '@models/accountModel'
+import { getRedis } from 'src/redisConfig'
 
 class AccountController {
   public async getAccount (req: Request, res: Response): Promise<Response> {
@@ -15,10 +16,15 @@ class AccountController {
     return res.json(account)
   }
 
-  public async getAccountBalance (req: Request, res: Response): Promise<Response> {
-    const accountBalance = await Account.findById(req.params.id, 'balance')
+  public async getAccountBalance (req: Request, res: Response) {
+    const accountId = req.params.id
 
-    return res.json(accountBalance)
+    const redisAccountBalance = await getRedis(`account-${accountId}`)
+    const cachedAccountBalance = JSON.parse(redisAccountBalance)
+
+    const accountBalance = await Account.findById(accountId, 'balance')
+
+    return res.json(cachedAccountBalance || accountBalance)
   }
 }
 
--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 import Transaction from '@models/transactionModel'
 import Account from '@models/accountModel'
+import { setRedis } from 'src/redisConfig'
 
 class TransactionController {
   public async getAllAccountTransactions (req: Request, res: Response): Promise<Response> {
@@ -24,6 +25,11 @@ class TransactionController {
     await Account.findByIdAndUpdate('625e09d9dea82d194642247e', {
       balance: balance + transactionValue
     })
+
+    const newBalance = await Account.findById(account.valueOf(), { balance: true, _id: false })
+    const newBalanceValue = JSON.stringify(JSON.parse(JSON.stringify(newBalance)).balance)
+
+    await setRedis(`account-${account.valueOf()}`, newBalanceValue)
 
     return res.json(transaction)
   }
