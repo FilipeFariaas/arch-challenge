06453c100f08730d0227115dc277cf32b5470b3b
FilipeFariaas <filipefarias117@gmail.com>
2022-04-19 23:06:24 -0300

    refactor: add try/catch

--- src/controllers/accountControler.ts
+++ src/controllers/accountControler.ts
@@ -41,22 +41,35 @@ class AccountController {
   }
 
   public async getAccountBalance (req: Request, res: Response): Promise<Response> {
-    const accountId = req.params.id
+    try {
+      const accountId = req.params.id
 
-    const redisAccountBalance = await getRedis(`account-${accountId}`)
+      const redisAccountBalance = await getRedis(`account-${accountId}`)
 
-    // if balance does not exist in cache, record balance in cache
-    if (!redisAccountBalance) {
-      const cachedAccountBalance = await Account.findById(accountId, { balance: true, _id: false })
+      if (!redisAccountBalance) {
+        const cachedAccountBalance = await Account.findById(accountId, { balance: true, _id: false })
 
-      return res.json(cachedAccountBalance)
-    } else {
-      const cachedAccountBalance = JSON.parse(redisAccountBalance)
+        return res.status(200).json({
+          status: 'success',
+          data: {
+            cachedAccountBalance
+          }
+        })
+      } else {
+        const cachedAccountBalance = JSON.parse(redisAccountBalance)
 
-      // const accountBalance = await Account.findById(accountId, 'balance')
-
-      // return res.json(cachedAccountBalance || accountBalance)
-      return res.json(cachedAccountBalance)
+        return res.status(200).json({
+          status: 'success',
+          data: {
+            cachedAccountBalance
+          }
+        })
+      }
+    } catch (error) {
+      return res.status(404).json({
+        status: 'fail',
+        message: error
+      })
     }
   }
 }
